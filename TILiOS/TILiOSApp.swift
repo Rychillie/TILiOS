1f1f6df2cba592166b21559da21445cedf52cbd1
Rychillie <rychillie@rychillie.net>
2025-04-23 17:08:17 -0300

    Initial Commit

---
+++ TILiOS/TILiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILiOSApp.swift
+//  TILiOS
+//
+//  Created by Rychillie Umpierre de Oliveira on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TILiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
